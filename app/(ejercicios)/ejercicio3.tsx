5cf98156c5910c892bf6b49fbe77d9daa4407985
Alvaro Reyes <alvarouchiha123@gmail.com>
2025-02-02 23:13:30 -0600

    ejercicio para ver un reloj en tiempo real, cada segundo va acxtualizando el valor de una variable que contendra la hora minutos y segundos actuales

--- app/(ejercicios)/ejercicio3.tsx
+++ app/(ejercicios)/ejercicio3.tsx
@@ -1,20 +1,42 @@
-import { ScrollView, Text, StyleSheet} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, ScrollView } from "react-native";
 
-export default function Ejericio3Screen() {
-  
+export default function Ejercicio3Screen() {
+    const [hora, setHora] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setHora(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const formatoHora = (hora:Date) => {        
+        return hora.toLocaleTimeString();
+    };
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <Text>Reloj en tiempo real </Text>
+            <Text style={styles.text}>Reloj en tiempo real</Text>
+            <Text style={styles.time}>{formatoHora(hora)}</Text>
         </ScrollView>
     );
-
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
-        justifyContent: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         padding: 20,
+    },
+    text: {
+        fontSize: 24,
+        marginBottom: 10,
+    },
+    time: {
+        fontSize: 32,
+        fontWeight: "bold",
     },
 });
