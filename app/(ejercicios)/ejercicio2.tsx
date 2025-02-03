68d0c3875143a79aec9f1392b9c3bd07b939b798
Alvaro Reyes <alvarouchiha123@gmail.com>
2025-02-02 23:12:09 -0600

    Ejercicio 2
    Ejercicio que funciona como contador, uno clickea y se va sumando un valor a una variable, al llegar a un multiplo de 5, se avisa que es un numero multiplo de 5

--- app/(ejercicios)/ejercicio2.tsx
+++ app/(ejercicios)/ejercicio2.tsx
@@ -1,20 +1,39 @@
-import { ScrollView, Text, StyleSheet} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, Button, StyleSheet, ScrollView, Alert } from "react-native";
 
-export default function Ejericio2Screen() {
-  
+export default function Ejercicio2Screen() {
+    const [contador, setContador] = useState(0);
+
+    useEffect(() => {
+        console.log("conteo actual: +"+contador);
+        
+        if (contador > 0 && contador % 5 === 0) {
+            Alert.alert("Informacion", "Ha alcanzado un múltiplo de 5");
+        }
+    }, [contador]);
+
+    const sumar = () => {
+        setContador(contador + 1)
+    };
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <Text>Contador </Text>
+            <Text style={styles.text}>Contador</Text>
+            <Button  title="Incrementar" onPress={sumar} />
         </ScrollView>
     );
-
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
-        justifyContent: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         padding: 20,
     },
+    text: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    
 });
