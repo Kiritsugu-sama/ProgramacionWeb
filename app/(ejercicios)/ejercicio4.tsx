3ea2d71062c102ea52ceb81209e541cde261122c
Alvaro Reyes <alvarouchiha123@gmail.com>
2025-02-02 23:14:23 -0600

    ejercicio que utiliza un settimeout para que la pantalla espere a que termine un proceso para cargar el mensaje de bienvenida

--- app/(ejercicios)/ejercicio4.tsx
+++ app/(ejercicios)/ejercicio4.tsx
@@ -1,20 +1,34 @@
-import { ScrollView, Text, StyleSheet} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, ScrollView } from "react-native";
 
-export default function Ejericio4Screen() {
-  
+export default function Ejercicio4Screen() {
+    const [cargando, setCargando] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setCargando(false);
+        }, 3000);
+
+        //comento esto por que no se si realmente es necesario limpiar el settimeout ya que se ejecuta una sola vez
+        //return () => clearTimeout(timeout);
+    }, []);
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <Text>Simulación de carga</Text>
+             <Text style={styles.text}>{ (cargando) ? "Cargando..." : "Bienvenido a la aplicación"}</Text>
         </ScrollView>
     );
-
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
-        justifyContent: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         padding: 20,
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
     },
 });
